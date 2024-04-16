dd1af6762cc55352c2dc83af2b721fd8288046cd
zhaoweilong <zhaoweilong176@gmail.com>
2024-04-16 21:30:57 +0800

    :tada: project init

---
+++ src/main/java/org/archive/CrawlerMain.java
@@ -0,0 +1,15 @@
+package org.archive;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ZhaoWeiLong
+ * @description
+ **/
+@Slf4j
+public class CrawlerMain {
+
+  public static void main(String[] args) {
+
+  }
+}
